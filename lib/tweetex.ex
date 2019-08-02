d5b651b0e73416fd2f77c6d94875f8109f55c321
Anis Tayebali <atayebali@gmail.com>
2019-08-02 06:18:01 -0700

    Made several methods private

--- lib/tweetex.ex
+++ lib/tweetex.ex
@@ -1,18 +1,18 @@
 defmodule Tweetex do
-  def creds do
+  defp creds do
     build_params |> OAuther.credentials      
   end
 
-  def sign(method, resource, params) do
+  defp sign(method, resource, params) do
     OAuther.sign(method, resource, params, creds)
   end
 
-  def build_request(method, resource, params) do
+  defp build_request(method, resource, params) do
     signature = sign(method, resource, params) 
     {header, req_params} = OAuther.header(signature)
     %{header: [header], params: req_params, method: method, resource: resource}   
   end
-  @doc """"
+ @doc """
     Perform calls twitter with: 
     method: http method
     resource: url and path
@@ -29,7 +29,7 @@ defmodule Tweetex do
     HTTPoison.get(request.resource, request.header, params: request.params)
   end
 
-  def build_params do
+  defp build_params do
     [
       consumer_key: Application.get_env(:tweetex, :consumer_key),
       consumer_secret: Application.get_env(:tweetex, :consumer_secret), 
