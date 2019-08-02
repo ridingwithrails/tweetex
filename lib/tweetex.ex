bf8e1dcdc97418fef7880443da215bdefb53809b
Anis Tayebali <atayebali@gmail.com>
2019-08-01 21:09:21 -0700

    Refactored perform a bit, add docs

--- lib/tweetex.ex
+++ lib/tweetex.ex
@@ -7,15 +7,26 @@ defmodule Tweetex do
     OAuther.sign(method, resource, params, creds)
   end
 
-  def request(method, resource, params) do
+  def build_request(method, resource, params) do
     signature = sign(method, resource, params) 
     {header, req_params} = OAuther.header(signature)
-    %{header: [header], params: req_params, method: method, resource: resource}
+    %{header: [header], params: req_params, method: method, resource: resource}   
   end
+  @doc """"
+    Perform calls twitter with: 
+    method: http method
+    resource: url and path
+    params: options to send 
 
-  def perform() do
-    HTTPoison.get(resource, [header], params: req_params)
-  
+    Returns {:ok, ${HTTPoison.Response}}
+
+    iex(8)> Tweetex.perform("get", "https://api.twitter.com/1.1/statuses/lookup.json", [{"id", 485086311205048320}])
+
+  """
+
+  def perform(method, resource, params) do
+    request = build_request(method, resource, params)
+    HTTPoison.get(request.resource, request.header, params: request.params)
   end
 
   def build_params do
