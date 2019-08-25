7416123f0618dfb3476b8b5f39e622640c93e6d9
Anis Tayebali <atayebali@gmail.com>
2019-08-25 14:24:01 -0700

    Added some interesting CLI capabilities

---
+++ lib/client.ex
@@ -0,0 +1,41 @@
+defmodule Tweetex.Client do
+	import Tweetex.Oauth
+	@doc """
+  Fetcher takes the options passed in from perform and makes the call to the API
+
+  Tweetex.fetcher("get", "statuses", "lookup", [{"id", 1156732168808218624}])
+
+  """
+  defp fetcher(method, object, action, params \\ []) do
+    resource = resource_builder(object, action)
+    request = build_request(method, resource, params)
+    HTTPoison.get(request.resource, request.header, params: request.params) 
+  end
+
+   @doc """
+    Perform calls twitter with: 
+    method: http method
+    resource: url and path
+    params: options to send 
+
+    Returns {:ok, ${HTTPoison.Response}}
+
+    iex(8)> Tweetex.perform("get", "statuses", "lookup", [{"id", 1156732168808218624}])
+
+  """
+
+  def perform(method, object, action, params \\ []) do
+    fetcher(method, object, action, params) |> deserializer
+  end
+
+  def resource_builder(object, action) do
+    Application.get_env(:tweetex, :base_url) <> "/" <> 
+    Application.get_env(:tweetex, :version) <> "/" <>
+      object <> "/" <> "#{action}.json"
+  end
+
+  defp deserializer(payload) do  
+    {:ok, twitter_response } =  payload 
+    twitter_response.body # |> Poison.decode 
+  end
+end
