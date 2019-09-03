34d6bd78357e79e1bde0cef52128cc67ddf0f05e
Anis Tayebali <atayebali@gmail.com>
2019-09-02 20:51:55 -0700

    Adding quick test file:

---
+++ test.sh
@@ -0,0 +1,5 @@
+echo "Get with params"
+./tweetex --method "get" --resource "favorites" --action "list" --params "[\"screen_name\", \"ridingwithrails\"]"
+
+echo "Get without params"
+./tweetex --method "get" --resource "trends" --action "available"
