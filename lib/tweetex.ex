087ab9cdc3228dc1a2c22e5a1e5c683f017a538b
Anis Tayebali <atayebali@gmail.com>
2019-09-11 22:49:20 -0700

    Added more tests, Refactored entry point

--- lib/tweetex.ex
+++ lib/tweetex.ex
@@ -3,7 +3,7 @@ defmodule Tweetex do
   import Tweetex.Oauth
 
   def api_client(), do: Application.get_env(:tweetex, :client)
-  
+
   @doc """
   main is the start of cli to hit twitter.
 
@@ -12,56 +12,66 @@ defmodule Tweetex do
   mix escript.build && ./tweetex --source "namegah"
 
   """
-  def main(args) do        
-    opts = OptionParser.parse(args, switches: [ method: :string, 
+  def main(args) do
+    opts = OptionParser.parse(args, switches: [ method: :string,
                                                 resource: :string,
-                                                action: :string,   
-                                                params: :string                                               
+                                                action: :string,
+                                                params: :string
                                                 ])
-    case opts do      
-      {[method: method, resource: resource, action: action, params: params],_,_} -> 
+    case opts do
+      {[method: method, resource: resource, action: action, params: params],_,_} ->
         #perform(method, resource, action,[{"id", 1156732168808218624}]) #TODO: I need to pass this in.
-        params = parse_params(params)               
-        perform(method, resource, action, params)
-      {[method: method, resource: resource, action: action],_,_} ->          
-          params = parse_params("")        
-          perform(method, resource, action, params)
+        performer(method, resource, action, params)
+      {[method: method, resource: resource, action: action],_,_} ->
+        performer(method, resource, action)
        #./tweetex --method "get" --resource "statuses" --action "lookup" --params "[\"id\", 2121]"
       {_,_,_} -> "In correct params sent"
     end
-    
   end
+
+  def performer(method, resource, action, params \\ "") do
+    params = parse_params(params)
+    perform(method, resource, action, params)
+  end
+
+  @doc """
+  Formats params that comes in to be sent to api
+
+    iex(2)> Tweetex.parse_params("[\\"ok\\", \\"Valid\\"]")
+    [{"ok", "Valid"}]
+
+    iex(4)> Tweetex.parse_params("")
+    []
+
+  """
 
   def parse_params(params \\ " ") do
     case Poison.decode(params) do
-      {:ok, data} -> 
-        data    
-        |> Enum.chunk_every(2) 
+      {:ok, data} ->
+        data
+        |> Enum.chunk_every(2)
         |> Enum.map(fn pair -> List.to_tuple(pair) end )
       {:error, _, _ } ->
-        []  
+        []
     end
   end
 
   @doc """
-    Perform calls twitter with: 
+    Perform calls twitter with:
     method: http method
     resource: url and path
-    params: options to send 
+    params: options to send
 
     Returns {:ok, ${HTTPoison.Response}}
-
-    ====iex(8)> Tweetex.perform("get", "statuses", "lookup", [{"id", 1156732168808218624}])
-    ==={:ok, ${HTTPoison.Response}}
   """
-  def perform(method, object, action, params \\ []) do    
+  def perform(method, object, action, params \\ []) do
     resource = resource_builder(object, action)
     request = build_request(method, resource, params)
     api_client.fetcher(method, request) |> deserializer
   end
 
-  defp deserializer(payload) do      
-    {:ok, twitter_response } =  payload 
+  defp deserializer(payload) do
+    {:ok, twitter_response } =  payload
     twitter_response.body  |> Poison.decode
   end
 end
--- test/tweet_ex_test.exs
+++ test/tweet_ex_test.exs
@@ -1,22 +1,31 @@
 defmodule TweetexTest do
   use ExUnit.Case
   import Mox
-  # doctest Tweetex
+  doctest Tweetex
 
   setup :verify_on_exit!
 
-  describe "Handling params" do     
+  describe "Handling CLI Args" do
     test "Bad options passed" do
       assert Tweetex.main([:nothing]) == "In correct params sent"
     end
 
     test "Options without params " do
       Tweetex.ClientBehaviorMock
-      |> expect(:fetcher, fn _method, _request ->
-       {:ok, %{body: "\"ok\""} }
-      end)
-      result = Tweetex.main(["--method", "get", "--resource", "statuses", "--action", "lookup"]) 
+        |> expect(:fetcher, fn _method, _request ->
+          {:ok, %{body: "\"ok\""} }
+        end)
+      result = Tweetex.main(["--method", "get", "--resource", "statuses", "--action", "lookup"])
       assert result == {:ok, "ok"}
-    end    
-  end  
+    end
+
+    test "Options with params" do
+      Tweetex.ClientBehaviorMock
+        |> expect(:fetcher, fn _method, _request ->
+          {:ok, %{body: "\"ok\""} }
+        end)
+      result = Tweetex.main(["--method",  "post", "--resource", "statuses", "--action", "update", "--params", "[\"status\", \"Been thinking about this alog\"]"])
+      assert result == {:ok, "ok"}
+    end
+  end
 end
