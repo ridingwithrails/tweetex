4de0e1f0b5ae94cebf986bcad7a3afc5a4d87d31
Anis Tayebali <atayebali@gmail.com>
2019-09-19 19:52:36 -0700

    Ability to split and join a huge file

--- lib/upload.ex
+++ lib/upload.ex
@@ -1,19 +1,24 @@
 defmodule Tweetex.Upload do
-	def upload(path)  do
-# 		'''
-# 		Get size of the file 
-# 		{:ok, %{size: size} } = File.stat "test.mp4"
+	def post(path)  do
+		File.stream!(".#{path}", [], 1024) 
+			|> Stream.chunk_every(1024)  
+			|> Stream.with_index 
+			|> Stream.each( 
+					fn({data, chunk_id}) -> {:ok, file} = File.open("./lib/output/#{chunk_id}.tmp",[:write] )
+					IO.binwrite(file, data)
+				end) 
+			|> Stream.run()
+	end
 
-
-		#Split it into chunks of 1 MB each
-
-
-
-File.stream!("./test.mp4", [], 1024) |> Stream.chunk_every(1024)  |> Stream.with_index |> Stream.each( 
-	fn({data, chunk_id}) -> {:ok, file} = File.open("./lib/output/#{chunk_id}.tmp",[:write] )
-	IO.binwrite(file, data)
-end) |> Stream.run()
-
-# 		'''
+	def join(path) do
+		{:ok, final_file} = File.open("./bigfinal.mp4", [:write])
+		{:ok, files} = File.ls("./lib/output/") 
+		file_count = Enum.count(files) 
+		filename_range = (0..(file_count -1) )
+		Enum.each(filename_range, fn file_name ->
+			{:ok, file} = File.read("./lib/output/#{file_name}.tmp")
+			IO.binwrite(final_file, file)			
+		end)
+		File.close(final_file)
 	end
 end
