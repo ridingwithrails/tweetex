dda2920d3860e97a90a0703d75ec974b44b5c475
Anis Tayebali <atayebali@gmail.com>
2019-09-28 12:23:14 -0700

    Added tmp folder to deal with file chunnks

--- lib/upload.ex
+++ lib/upload.ex
@@ -80,27 +80,4 @@ defmodule Tweetex.Upload do
 			]}
 		HTTPoison.post(request.resource, form, request.header, params: request.params) |> IO.inspect		
 	end
-
-	def split(file)  do
-		File.stream!("#{@stage}/#{file}", [],  4999990) 
-			# |> Stream.chunk_every(5120)  
-			|> Stream.with_index 
-			|> Stream.each( 
-					fn({data, chunk_id}) -> {:ok, file} = File.open("#{@output}/#{chunk_id}.tmp",[:write] )
-					IO.binwrite(file, data)
-				end) 
-			|> Stream.run()
-	end
-
-	def join(finalfile) do
-		{:ok, finished_file} = File.open("./#{finalfile}.mp4", [:write])
-		{:ok, files} = File.ls("#{@output}") 
-		file_count = Enum.count(files) 
-		filename_range = (0..(file_count - 1) )
-		Enum.each(filename_range, fn file_name ->
-			{:ok, file} = File.read("#{@output}/#{file_name}.tmp")
-			IO.binwrite(finished_file, file)			
-		end)
-		File.close(finished_file)
-	end
 end
