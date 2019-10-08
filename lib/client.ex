0a3cfed4d79aadaa3dd60e73ce50046e354a3a80
Anis Tayebali <atayebali@gmail.com>
2019-10-07 21:09:37 -0700

    Cleaned up a redundant code

--- lib/client.ex
+++ lib/client.ex
@@ -3,17 +3,11 @@ defmodule Tweetex.Client do
   @doc """
   Fetcher takes the options passed in from perform and makes the call to the API
 
+  Fetcher also handles media upload -- note the multi part form that can be passed.
+
   Tweetex.Client.fetcher("get", "statuses", "lookup", [{"id", 1156732168808218624}])
 
-  """
-  # def fetcher(method, request) do
-  #   case method do
-  #    "get" -> 
-  #       HTTPoison.get(request.resource, request.header, params: request.params) 
-  #    "post" -> 
-  #       HTTPoison.post(request.resource, [], request.header, params: request.params) 
-  #    end
-  #  end   
+  """ 
   def fetcher(method, request, form \\ []) do
     case method do
       "get" -> 
@@ -22,14 +16,5 @@ defmodule Tweetex.Client do
          HTTPoison.post(request.resource, {:multipart, form}, request.header, params: request.params)
      end
   end
-
-   def uploader(method, request, form \\ []) do
-     case method do
-       "get" -> 
-          HTTPoison.get(request.resource, request.header, params: request.params) 
-       "post" ->           
-          HTTPoison.post(request.resource, {:multipart, form}, request.header, params: request.params)
-      end
-   end
 end
 
