147313d26cf3ae55412f1c1fc52c6e11599e5aa2
Anis Tayebali <atayebali@gmail.com>
2019-10-25 22:29:32 -0700

    WIP looping until media string is produced

--- lib/chunked_uploader.ex
+++ lib/chunked_uploader.ex
@@ -7,6 +7,24 @@ defmodule Tweetex.ChunkedUploader do
 		media_id = Map.get media_data, "media_id"
 		split(file, media_id)
 		finalize(media_id)
+		find_status(media_id)
 		# Need to implement a status mechanism here.
+	end
+
+	def find_status(media_id) do
+		Process.sleep(1000)
+		media_data = status(media_id)
+		case processing?(media_data) do
+			true  -> find_status(media_data)
+			false -> media_data
+		end
+	end
+	
+	def processing?(media_data) do 
+		{:ok, data} = media_data
+		state = data 
+			|> Map.get("processing_info") 
+			|> Map.get("state")
+			state == "succeeded" 
 	end
 end
